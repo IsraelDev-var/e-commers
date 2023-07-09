7946d803cb6a7e4866814bc937aa08e0c15df1c4
Israel <israel230804vc@gmail.com>
2023-07-09 03:33:57 -0400

    carrito teminado

--- src/style/js/main.js
+++ src/style/js/main.js
@@ -103,9 +103,61 @@ function exitCart(){
 
 }
 
-function lookingCArd(db){
+function printProductsIncart(db){
+    let html = "";
+            
+    for (const key in db.cart) {
+        const {amount,name,image,price,id,quantity} = db.cart[key];
+        html += `
+        <div class="cart__product">
+            <div class="cart__product__img">
+                <img src="${image}" alt=""/>
+            </div>
+        
+
+            <div class="cart__product__body">
+                <p>
+                    <b>${name}</b>
+                </p>
+
+                <p>
+                    <small>Price: ${price} | Amount: ${amount}</small>
+                </p>
+                <p class="quantity">
+                <b>
+                    <small>Stock ${quantity}</small>
+                </b>
+                </p>
+
+                <p>
+                    <b>
+                        $<small>${amount * price}</small>
+                    </b>
+                </p>
+
+                <di class="cart__product__opt" id="${id}">
+                    <i class='bx bx-plus btn__product cart__opt' ></i>
+                    <i class='bx bx-minus'></i>
+                    <i class='bx bxs-trash'></i>
+                    
+                </di>
+            </div>
+
+        </div>
+        `;
+
+        
+    }
+    document.querySelector(".cart__products").innerHTML = html;
+
+
+
+}
+
+function addToCArd(db){
     const productsHTML = document.querySelector(".products")
     productsHTML.addEventListener("click", (e) => {
+        
     
         if(e.target.classList.contains("btn__product")){
             const id = Number(e.target.id);
@@ -113,15 +165,26 @@ function lookingCArd(db){
             const productFuond = db.products.find( function(product){
                 return product.id === id;
             })
+
+
             if(db.cart[productFuond.id]){
-                db.cart[productFuond.id].amount++
+                if( db.cart[id].amount === db.cart[id].quantity){
+                
+                    alert(" ARTICULO AGOTADOS");
+                }else{
+                    db.cart[id].amount++;
+                    
+                }
             }else{
                 db.cart[productFuond.id] = {
                     ...productFuond,
                     amount: 1
                 }
             }
-            console.log(db.cart)
+
+            localStorage.setItem("cart", JSON.stringify(db.cart));
+            printProductsIncart(db)
+        
         }
 
 
@@ -130,20 +193,71 @@ function lookingCArd(db){
 
 }
 
+function cartValidetion (){
+    document.querySelector(".cart__products").addEventListener("click", (e)=> {
+        if(e.target.classList.contains("bx")){
+            if(e.target.classList.contains("bx-minus")){
+                const id = Number(e.target.parentElement.id);
+                
+
+                if(db.cart[id].amount === 1){
+                    delete db.cart[id];
+                }else{
+                    db.cart[id].amount--;
+                }
+
+                
+            }
+            if(e.target.classList.contains("bx-plus")){
+                const id = Number(e.target.parentElement.id);
+                const quantity = Number(db.cart[id].quantity);
+                console.log(quantity);
+                if( db.cart[id].amount === quantity){
+                    
+                    alert(" ARTICULO AGOTADOS");
+                }else{
+                    db.cart[id].amount++;
+                    
+                    
+                }
+                
+                
+            }
+            if(e.target.classList.contains("bxs-trash")){
+                const id = Number(e.target.parentElement.id);
+                const anwer = confirm("DESEA ELIMINAR TODO");
+
+                if(anwer){
+                    delete db.cart[id];
+                }else{
+
+                }
+                
+            }
+            localStorage.setItem("cart", JSON.stringify(db.cart));
+            printProductsIncart(db);
+            
+        
+}})
+
+}
+
 async function main(){
     const db = {
         products: 
         JSON.parse(window.localStorage.getItem("products")) || await getProducts(),
-        cart:{},
+        cart:JSON.parse(window.localStorage.getItem("cart")) || {},
     }
+
     showMenu()
-    lookingCArd(db)
+    addToCArd(db)
     printProducts(db);
     showCart();
-    showHeader()
-    exitCart()
-    exitMenu()
-    
+    showHeader();
+    exitCart();
+    exitMenu();
+    printProductsIncart(db);
+    cartValidetion (db);
 
 
 }
